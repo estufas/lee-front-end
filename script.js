b7bac83ae59ce6e32a9fb161b785c742df9ca9e1
Stuart Hiller <hiller.stuart@gmail.com>
2015-08-24 18:38:26 -0700

    Basic skeleton

---
+++ script.js
@@ -0,0 +1,5 @@
+var newSubmission = document.getElementById('usrform');
+newSubmission.addEventListener("click", function(e) {
+  var name = document.getElementById('usrname').value;
+  var comment = document.getElementById('comment').value;
+})
