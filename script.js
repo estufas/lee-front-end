5124d5fa18f61ac60bfc08e9c36c4bdd8cfbc6bd
Stuart Hiller <hiller.stuart@gmail.com>
2015-08-28 13:59:31 -0700

    Needs fine tuning 2

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-var UserComment = function(user, comment) {
-  this.user = user;
+var UserComment = function(comment) {
   this.comments = [];
 
   if(comment) {
@@ -16,25 +15,23 @@ var submission = [];
 
 $("#usrform").on('submit', function(e){
    e.preventDefault();
-   var userName = $("#usrname").val();
    var userComment = $("#comment").val();
-   if (!(userName && userComment)) {
+   if (!(userComment)) {
 
      console.log("its empty")
      return;
    };
-   console.log(userComment,userName);
-   submission.push(new UserComment(userName, userComment));
+   console.log(userComment);
+   submission.push(new UserComment(userComment));
    console.dir(submission);
    renderCommets(submission.length - 1);
  });
 
 var renderCommets = function(x) {
   // for (var i = 0; i < submission.length; i++) {
-    console.log(submission[x].user);
-    var string = '<p>' + submission[x].user + '</p>';
-    var string2 = '<p>' + submission[x].comments + '</p>';
-  $("#submissions").append(string, string2);
+    console.log(submission[x].comments);
+    var string = '<p>' + submission[x].comments + '</p>';
+  $("#submissions").append(string);
 // }
 };
 
